a83ff96f15dacd921c1f028a7f7228ca8669b5f9
Jayesh <jchamoli182@gmail.com>
2025-01-11 05:41:01 -0800

    test2 branch commit

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ class Main extends LC1{
     public static void main(String[] args) {
         // LC1 l = new LC1("chamoli");
         String s = "";
-        LC1 lc = new LC1();
-        // helper();
+        // calling the default constructor here:
+        LC1 lc = new LC1();                
+        // helper();  ----> cannot call a non static method inside a static method
         lc.createString();
 
         System.out.println("returning a empty string: " + s);
